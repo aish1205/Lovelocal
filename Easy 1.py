9a037a78445caf642aa833798e90e06fe8746d6c
Aishwarya Kulkarni <110886659+aish1205@users.noreply.github.com>
2023-12-06 13:29:36 +0530

    Easy 1.py

--- Easy 1.py
+++ Easy 1.py
@@ -1,6 +1,17 @@
-s= 'Hello world'
-print(len(s.strip().split()[-1]))
-s= 'fly me to the moon'
-print(len(s.strip().split()[-1]))
-s= 'luffy is still joyboy'
-print(len(s.strip().split()[-1]))
+def length_of_last_word(s):
+    return len(s.strip().split()[-1])
+  
+# Example 1
+s = "Hello World"
+last_word_length = length_of_last_word(s)
+print(last_word_length)  # Output: 5
+
+# Example 2
+s = "   fly me   to   the moon  "
+last_word_length = length_of_last_word(s)
+print(last_word_length)  # Output: 4
+
+# Example 3
+s = "luffy is still joyboy"
+last_word_length = length_of_last_word(s)
+print(last_word_length)  # Output: 6
