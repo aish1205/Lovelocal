dceaf7af24e95397fa9209e6b2a3b02e74c2b8da
Aishwarya Kulkarni <110886659+aish1205@users.noreply.github.com>
2023-12-06 13:21:20 +0530

    hard 3.py

--- hard 3.py
+++ hard 3.py
@@ -1,13 +1,11 @@
 def countDigitOne(n):
-    # Initialize the count of digit 1
+  
     count = 0
 
-    # Iterate from 1 to n
+   
     for i in range(1, n + 1):
-        # Convert the current number to a string
         num_str = str(i)
 
-        # Count the number of occurrences of '1' in the string
         count += num_str.count('1')
 
     return count
